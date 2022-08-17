77eea130cb976f41963eaa68c9b46f0cbe842a37
yarahman <yarahman@gmail.com>
2022-08-17 17:06:06 +0600

    adding animation to adding product screen

--- lib/screens/android/adding_product_screen.dart
+++ lib/screens/android/adding_product_screen.dart
@@ -17,7 +17,50 @@ class _AddingProductScreenState extends State<AddingProductScreen>
     with TickerProviderStateMixin {
   //
   //
-  //* global variables --------->
+  //*@@@@@@@@@@@@@@@@@@@@@@@@@@@@ all common variables @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+  var dropDownValue = 'min';
+  var fileUploadButtonText = 'Upload File';
+  var uploadFileIcon = Icons.file_copy;
+  bool isSelectionMode = false;
+
+//
+//?####################################### end commons Variables ###############################
+//
+
+//
+//*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ start all animations variables @@@@@@@@@@@@@@@@@@@@@@@@@@
+  AnimationController? categoryController;
+  Animation? catagoryTextColorAnim;
+  //
+  //*this is for upload product image or videos button animations
+  //
+  AnimationController? uploadImageOrVideoButtonController;
+  Animation<double>? buttonHightAnimation;
+  //
+  //* this is for upload file button text animation
+  AnimationController? uploadFileTextController;
+  Animation<double>? uploadFileTextAnimation;
+  //
+  //* upload file button icon animation
+  AnimationController? uploadFileIconController;
+  Animation<double>? uploadFileIconAnimation;
+  //
+  //
+  //*this is 1st animation for delete button
+  AnimationController? deleteButtonOneController;
+  Animation<double>? deleteOneAnimation;
+  //
+  //* this is secound animation for delete button
+  AnimationController? deleteButtonTwoController;
+  Animation<double>? deleteTwoAnimation;
+//
+//?#################################### end all animations variabes ############################
+//
+//
+//*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ all lists @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  List<File> imagesOrVideos = [];
+  List<File> savedImagesOrVidoes = [];
   List deliveryTimes = [
     'min',
     'hour',
@@ -39,20 +82,12 @@ class _AddingProductScreenState extends State<AddingProductScreen>
   ];
 
   List<String> selectedCategorys = [];
+  List<File> selectedFiles = [];
 
-  var dropDownValue = 'min';
-
-  AnimationController? animationController;
-  Animation? catagoryTextColorAnim;
-
-  List<File> imagesOrVideos = [];
-  List<File> savedImagesOrVidoes = [];
-
+//?#################################### end all lists #################################
   //
   //
-  //
-  //* global methods(own)----------->
-
+  //*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ own methods @@@@@@@@@@@@@@@@@@@@@@@@
   Future<void> pickImageOrVideo() async {
     FilePickerResult? result = await FilePicker.platform
         .pickFiles(allowMultiple: true, type: FileType.media);
@@ -61,6 +96,7 @@ class _AddingProductScreenState extends State<AddingProductScreen>
       imagesOrVideos = result.paths.map((path) {
         setState(() {
           savedImagesOrVidoes.add(File(path!));
+          uploadImageOrVideoButtonController!.forward();
         });
 
         return File(path!);
@@ -68,21 +104,35 @@ class _AddingProductScreenState extends State<AddingProductScreen>
     }
   }
 
-  //
-  //
-  //
-  //*flutter default methods ------------>
+//
+//
+  void removeSelectedImage() {
+    setState(() {
+      savedImagesOrVidoes.removeWhere((files) {
+        return selectedFiles.contains(files);
+      });
+    });
+  }
 
+//
+//?##################################end own methods #####################################
+  //
+  //
+  //
+  //*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ flutter default methods @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   @override
   void initState() {
-    animationController = AnimationController(
+    //
+    //
+    //* this is for Select Product Catagory Animation---->
+    categoryController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
     );
     catagoryTextColorAnim =
         ColorTween(begin: Colors.white, end: Colors.black).animate(
       CurvedAnimation(
-        parent: animationController!,
+        parent: categoryController!,
         curve: Curves.easeInBack,
       ),
     );
@@ -90,8 +140,126 @@ class _AddingProductScreenState extends State<AddingProductScreen>
     catagoryTextColorAnim!.addListener(() {
       setState(() {});
     });
+
+    //
+    //
+    //* this is for Upload Image Or Video Button Animations
+    uploadImageOrVideoButtonController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 800,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          uploadFileTextController!.forward();
+          uploadFileIconController!.forward();
+        }
+      });
+    buttonHightAnimation = Tween<double>(
+      begin: 150,
+      end: 40,
+    ).animate(uploadImageOrVideoButtonController!);
+
+//
+//
+//
+//* this animation for upload file text convert to add more text
+
+    uploadFileTextController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 500,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          uploadFileTextController!.reverse();
+          setState(() {
+            fileUploadButtonText = 'Add More';
+          });
+        }
+      });
+
+    uploadFileTextAnimation =
+        Tween<double>(begin: 0.0, end: -200).animate(uploadFileTextController!);
+
+    //*this is for upload file button icon changes animation
+    //
+    //
+    uploadFileIconController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 500,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          uploadFileIconController!.reverse();
+          setState(() {
+            uploadFileIcon = Icons.add_a_photo_outlined;
+          });
+        }
+      });
+    uploadFileIconAnimation = Tween<double>(
+      begin: 0.0,
+      end: 200,
+    ).animate(uploadFileIconController!);
+
+    //
+    //
+    //* this is for 1st delete button animation
+    deleteButtonOneController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          deleteButtonTwoController!.repeat();
+        }
+      });
+    deleteOneAnimation = Tween<double>(begin: -200, end: 0.0)
+        .animate(deleteButtonOneController!);
+    //
+    //* this is for 2nd delete button animation
+    deleteButtonTwoController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 700,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+    deleteTwoAnimation = Tween<double>(begin: 24.0, end: 35).animate(
+        CurvedAnimation(
+            parent: deleteButtonTwoController!, curve: Curves.bounceIn));
     super.initState();
   }
+
+  //
+  //
+  @override
+  void dispose() {
+    categoryController!.dispose();
+    uploadImageOrVideoButtonController!.dispose();
+    uploadFileTextController!.dispose();
+    uploadFileIconController!.dispose();
+    deleteButtonOneController!.dispose();
+    deleteButtonTwoController!.dispose();
+
+    super.dispose();
+  }
+  //?############################################# end of flutter  default methods ##########################
 
   @override
   Widget build(BuildContext context) {
@@ -100,6 +268,31 @@ class _AddingProductScreenState extends State<AddingProductScreen>
         title: const Text(
           'Add Your Products',
         ),
+        actions: [
+          if (isSelectionMode)
+            Transform.translate(
+              offset: Offset(deleteOneAnimation!.value, 0.0),
+              child: IconButton(
+                onPressed: () async {
+                  removeSelectedImage();
+                  setState(() {
+                    selectedFiles.clear();
+                  });
+                  deleteButtonTwoController!.stop();
+                  deleteButtonOneController!.reverse();
+                  Future.delayed(const Duration(seconds: 1)).then((_) {
+                    setState(() {
+                      isSelectionMode = false;
+                    });
+                  });
+                },
+                icon: Icon(
+                  Icons.delete_forever,
+                  size: deleteTwoAnimation!.value,
+                ),
+              ),
+            ),
+        ],
       ),
       body: SafeArea(
         child: ListView(
@@ -232,7 +425,7 @@ class _AddingProductScreenState extends State<AddingProductScreen>
                     if (!selectedCategorys.contains(value)) {
                       setState(() {
                         selectedCategorys.add(value);
-                        animationController!.repeat();
+                        categoryController!.repeat();
                       });
                     } else {
                       setState(() {
@@ -285,7 +478,7 @@ class _AddingProductScreenState extends State<AddingProductScreen>
               ),
               child: Container(
                 alignment: Alignment.center,
-                height: 150,
+                height: buttonHightAnimation!.value,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(
@@ -299,55 +492,154 @@ class _AddingProductScreenState extends State<AddingProductScreen>
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextButton(
-                      onPressed: pickImageOrVideo,
-                      child: const Text(
-                        'Upload File',
-                        style: TextStyle(
+                    Transform.translate(
+                      offset: Offset(uploadFileTextAnimation!.value, 0.0),
+                      child: TextButton(
+                        onPressed: pickImageOrVideo,
+                        child: Text(
+                          fileUploadButtonText,
+                          style: const TextStyle(
                             fontFamily: Style.ard,
                             fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 65, 65, 65)),
+                            color: Color.fromARGB(
+                              255,
+                              65,
+                              65,
+                              65,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                    const Icon(
-                      Icons.file_copy,
+                    Transform.translate(
+                      offset: Offset(uploadFileIconAnimation!.value, 0.0),
+                      child: Icon(
+                        uploadFileIcon,
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
             Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                ),
-                child: savedImagesOrVidoes.isNotEmpty
-                    ? GridView(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 4,
-                          childAspectRatio: 3 / 2,
-                          mainAxisExtent: 150,
-                          crossAxisSpacing: 3,
-                          mainAxisSpacing: 5,
-                        ),
-                        children: savedImagesOrVidoes.map((photoOrVidoes) {
-                          if (photoOrVidoes.path.endsWith('.jpg') ||
-                              photoOrVidoes.path.endsWith('.png')) {
-                            return Image.file(
-                              photoOrVidoes,
-                              fit: BoxFit.cover,
-                            );
-                          }
-                          return Container(
-                            height: 150,
-                            color: Colors.green,
-                            child: const Text('video'),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+              ),
+              child: savedImagesOrVidoes.isNotEmpty
+                  ? GridView(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        childAspectRatio: 3 / 2,
+                        mainAxisExtent: 150,
+                        crossAxisSpacing: 3,
+                        mainAxisSpacing: 5,
+                      ),
+                      children: savedImagesOrVidoes.map((photoOrVidoes) {
+                        if (photoOrVidoes.path.endsWith('.jpg') ||
+                            photoOrVidoes.path.endsWith('.png')) {
+                          return InkWell(
+                            onTap: () async {
+                              if (isSelectionMode) {
+                                setState(() {
+                                  if (!selectedFiles.contains(photoOrVidoes)) {
+                                    selectedFiles.add(photoOrVidoes);
+                                  } else {
+                                    selectedFiles.remove(photoOrVidoes);
+                                    if (selectedFiles.isEmpty) {
+                                      Future.delayed(const Duration(seconds: 1))
+                                          .then(
+                                        (_) {
+                                          setState(() {
+                                            isSelectionMode = false;
+                                          });
+                                        },
+                                      );
+
+                                      deleteButtonTwoController!.stop();
+                                      deleteButtonOneController!.reverse();
+                                    }
+                                  }
+                                });
+                              }
+                            },
+                            onLongPress: () {
+                              setState(() {
+                                selectedFiles.add(photoOrVidoes);
+                                isSelectionMode = true;
+                              });
+                              deleteButtonOneController!.forward();
+                            },
+                            child: Stack(
+                              alignment: Alignment.topRight,
+                              children: [
+                                Image.file(
+                                  photoOrVidoes,
+                                  fit: BoxFit.cover,
+                                ),
+                                if (selectedFiles.contains(photoOrVidoes))
+                                  const Icon(
+                                    Icons.check_circle,
+                                    color: Style.defaultColor,
+                                  ),
+                              ],
+                            ),
                           );
-                        }).toList(),
-                      )
-                    : null,),
+                        }
+                        return InkWell(
+                          onTap: () async {
+                            if (isSelectionMode) {
+                              setState(() {
+                                if (!selectedFiles.contains(photoOrVidoes)) {
+                                  selectedFiles.add(photoOrVidoes);
+                                } else {
+                                  selectedFiles.remove(photoOrVidoes);
+                                  if (selectedFiles.isEmpty) {
+                                    Future.delayed(const Duration(seconds: 1))
+                                        .then(
+                                      (_) {
+                                        setState(() {
+                                          isSelectionMode = false;
+                                        });
+                                      },
+                                    );
+
+                                    deleteButtonTwoController!.stop();
+                                    deleteButtonOneController!.reverse();
+                                  }
+                                }
+                              });
+                            }
+                          },
+                          onLongPress: () {
+                            setState(() {
+                              selectedFiles.add(photoOrVidoes);
+                              isSelectionMode = true;
+                            });
+                            deleteButtonOneController!.forward();
+                          },
+                          child: Stack(
+                            alignment: Alignment.topRight,
+                            children: [
+                              Container(
+                                height: 150,
+                                color: Colors.green,
+                                child: const Text('video'),
+                              ),
+                              if (selectedFiles.contains(photoOrVidoes))
+                                const Icon(
+                                  Icons.check_circle,
+                                  color: Style.defaultColor,
+                                ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    )
+                  : null,
+            ),
           ],
         ),
       ),
